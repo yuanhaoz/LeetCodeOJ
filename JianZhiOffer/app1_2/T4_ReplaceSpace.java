aba3e228ce83bf643d3cac3422950cf9eea803f1
yuanhao <994303>
2017-04-27 10:22:33 +0800

    修改了工程名

--- JianZhiOffer/app1_2/T4_ReplaceSpace.java
+++ JianZhiOffer/app1_2/T4_ReplaceSpace.java
@@ -22,8 +22,9 @@ public class T4_ReplaceSpace {
 		string[10] = 'p';
 		string[11] = ' ';
 
-		int length = replaceBlank(string, 12);
-		System.out.println(new String(string, 0, length));
+//		int length = replaceBlank(string, 12);
+        int length = replaceBlank2(string, 12);
+        System.out.println(new String(string, 0, length));
 
 
 		char[] string1 = new char[50];
@@ -95,6 +96,49 @@ public class T4_ReplaceSpace {
 
 		return temp;
 	}
+
+    /**
+     * 请实现一个函数，将一个字符串中的空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+     *
+     * @param string     要转换的字符数组，长度远大于usedLength，方便存储改变后的字符串
+     * @param usedLength 字符数组中已经使用的长度
+     * @return 转换后使用的字符长度，-1表示处理失败
+     */
+    public static int replaceBlank2(char[] string, int usedLength) {
+        if (string == null || string.length < usedLength) {
+            return -1;
+        }
+
+        int blankNum = 0; // 得到空格数
+        for (int i = 0; i < usedLength; i++) {
+            if (string[i] == ' ') {
+                blankNum++;
+            }
+        }
+        int newLength = usedLength + 2 * blankNum; // 新字符串长度
+        if (blankNum == 0) { // 没有空格则直接返回
+            return newLength;
+        }
+        if (newLength > string.length) { // 新串的长度大于字符数组的长度则返回-1
+            return -1;
+        }
+        // 替换字符数组
+        int i = usedLength - 1;
+        int j = newLength - 1;
+        while (i >= 0 && j >= 0 && i < j) {
+            if (string[i] != ' ') {
+                string[j] = string[i];
+                i--;
+                j--;
+            } else {
+                string[j--] = '0';
+                string[j--] = '2';
+                string[j--] = '%';
+                i--;
+            }
+        }
+        return newLength;
+    }
 
 	/**
 	 * 一个字符串，删除其中所有的'a'，把所有的'b'用'cc'替换。不能用额外的辅助空间，a的个数大于b的个数
