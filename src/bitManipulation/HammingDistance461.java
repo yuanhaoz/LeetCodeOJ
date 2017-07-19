e2ee98f70befc1b007ef38fd11d5540ca3febf5d
郑元浩 <zhengyuanhao@guazi.com>
2017-07-19 22:21:39 +0800

    添加位运算，求海明距离，使用异或运算，并计算异或结果有多少个1

---
+++ src/bitManipulation/HammingDistance461.java
@@ -0,0 +1,18 @@
+package bitManipulation;
+
+public class HammingDistance461 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+			count += (xor >> i) & 1;
+		}
+        return count;
+    }
+
+}
